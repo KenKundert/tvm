ffd66c9fc192b422b5743ff1032a98f36dff616c
Ken Kundert <ken@theKunderts.net>
2019-04-04 12:19:01 -0700

    bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as file:
 
 setup(
     name = 'tvm',
-    version = '0.0.2',
+    version = '0.0.3',
     author = 'Ken Kundert',
     author_email = 'tvm@nurdletech.com',
     description = 'Time Value of Money',
